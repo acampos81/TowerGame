2f35109406e7192035fa17e3b5de076ae6429a6d
Angel Campos <acampos81@gmail.com>
2025-08-02 09:50:03 -0600

    WIP spawning

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,27 +1,40 @@
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
 {
   public int enemy1Count;
   public int enemy2Count;
-  public Transform spawnPoint1;
-  public Transform spawnPoint2;
-  public Transform spawnPoint3;
+
+  [Tooltip("How often enemies will spawn in seconds.")]
+  public float spawnInterval;
+  
+  public List<Transform> spawnPoints;
+
+
+  private GameObject _enemy1Prefab;
+  private GameObject _enemy2Prefab;
+  private float _elapsedSpawnTime;
+  private int _spawnIndex;
+  private int _enemy1Spawned;
+  private int _enemy2Spawned;
 
   // Start is called once before the first execution of Update after the MonoBehaviour is created
   void Start()
   {
-    var enemy1 = Resources.Load("Enemy1") as GameObject;
-    SpawnEnemies(enemy1Count, enemy1, spawnPoint1);
-    SpawnEnemies(enemy1Count, enemy1, spawnPoint2);
-    SpawnEnemies(enemy1Count, enemy1, spawnPoint3);
+    _enemy1Prefab = Resources.Load("Enemy1") as GameObject;
+    _enemy2Prefab = Resources.Load("Enemy2") as GameObject;
   }
 
   // Update is called once per frame
   void Update()
   {
+    if(_enemy1Spawned < enemy1Count)
+    {
+      CheckSpawnInterval(Time.deltaTime);
+    }
   }
 
   public void RestartLevel()
@@ -30,18 +43,37 @@ public class LevelManager : MonoBehaviour
     SceneManager.LoadScene(scene.name, LoadSceneMode.Single);
   }
 
-  private void SpawnEnemies(int count, GameObject enemyPrefab, Transform spawnPoint)
+  private void CheckSpawnInterval(float deltaTime)
   {
-    for(int i=0; i < count; i++)
+    _elapsedSpawnTime += deltaTime;
+    if(_elapsedSpawnTime >= spawnInterval)
     {
-      var enemyInstance = GameObject.Instantiate(enemyPrefab);
-      enemyInstance.transform.position = spawnPoint.position;
-      
-      var agentLocation = spawnPoint.position;
-      agentLocation.x = 30;
-      
-      var agent = enemyInstance.GetComponent<NavMeshAgent>();
-      agent.SetDestination(agentLocation);
+      var spawnPoint = spawnPoints[_spawnIndex];
+
+      SpawnEnemy(_enemy1Prefab, spawnPoint);
+
+      _enemy1Spawned++;
+
+      _elapsedSpawnTime = 0.0f;
+
+      _spawnIndex++;
+
+      if(_spawnIndex == spawnPoints.Count)
+      {
+        _spawnIndex = 0;
+      }
     }
+  }
+
+  private void SpawnEnemy(GameObject enemyPrefab, Transform spawnPoint)
+  {
+    var enemyInstance = GameObject.Instantiate(enemyPrefab);
+    enemyInstance.transform.position = spawnPoint.position;
+      
+    var agentLocation = spawnPoint.position;
+    agentLocation.x = 30f;
+      
+    var agent = enemyInstance.GetComponent<NavMeshAgent>();
+    agent.SetDestination(agentLocation);
   }
 }
